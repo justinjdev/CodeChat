438f454ce7e43442fec1a06fa43abe7046e8c985
Cameron Green <cgree78@gmail.com>
2017-11-08 12:12:42 -0600

    Parser connected to the server.

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,119 +1,61 @@
-'use strict' //pls
-
 const express = require('express')
 const app = express()
-const http = require('http')
+const http = require('https')
 
-
-app.set('port', process.env.PORT || 8080)
-
+app.set('port', process.env.PORT || 3000)
 
 let server = app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'))
 })
 
-// creation of variables. Socket.io listening at "server" which I'll just make
-// localhost/serverAddress
 const io = require('socket.io').listen(server)
-let guestNumber = 1
-let nickNames = {}
-let namesUsed = []
-let name
-let nameIndex
+const socketio = require('socket.io-client')('http://localhost:3001')
 
-/**
- * @param {object} socket
- *
- * Small explanation of socket vs io:
- *
- **** Read up on the technologies here: https://socket.io/
- *
- * Sockets send a string + an object for every emit to the server and the server does
- * whatever the hell it wants with that object. In chats it usually forwards it to whatever room the socket is in.
- *
- *
- * On the io connection the whole function fires with the paramater of the socket.
- * socket.on/socket.emit means sending to the specific socket that it is connected to.
- */
-io.on('connection', (socket) => {
-  // when they connect to the server, default to lobby channel
-  socket.join('Lobby')
-  // when they join, emit the message 'joinResult'
-  socket.emit('joinResult', {
-    room: 'Lobby'
-  }) //let the client know that it's defaulted to the lobby
+const Parser = require('./parser')
+const parser = new Parser(io)
 
-  /**
-   * when the socket connection disconnects then you remove the nickname and name used from the list of names used
-   */
+io.on('connection', socket => {
+  console.log(socket.id, 'connected')
+  // parser.readInput(socket)
+  socketio.emit('message', 'hello we are back end!')
+
   socket.on('disconnect', () => {
-    nameIndex = namesUsed.indexOf(nickNames[socket.id])
-    delete namesUsed[nameIndex] // I really don't like using that explicitly but if it works...
-    delete nickNames[socket.id]
+    console.log(socket.id, 'disconnected')
   })
-
-  // create variable name = 'Guest' + number, then save that in the object
-  // {socket.id: name}
-  name = 'Guest' + guestNumber
-  nickNames[socket.id] = name
-  socket.emit('nameResult', { //then tell the client that the namechange was successful and go for it
-    success: true,
-    name: name
-  })
-  socket.emit('message', {
-    message: `Welcome ${name}`
-  })
-  namesUsed.push(name) //add new nickname to the names used list, (note: no way to remove from the list)
-  guestNumber += 1
-
-  socket.on('nameAttempt', (name) => {
-    if (name.indexOf('Guest') == 0) {
-      socket.emit('nameResult', {
-        success: false,
-        message: 'Names cannot begin with "Guest".'
-      })
+  socket.on('message', message => {
+    if (message.text[0] === '/') {
+      parser.readInput(message)
     } else {
-      // if the name was not in the list, change the nickname and add to the list
-      if (namesUsed.indexOf(name) == -1) {
-        namesUsed.push(name)
-        nickNames[socket.id] = name
-        dblink.saveUsername(name)
-        socket.emit('nameResult', {
-          success: true,
-          name: name
-        })
-      } else {
-        socket.emit('nameResult', {
-          success: false,
-          message: 'That name is already in use.'
-        })
-      }
+      socket
+        .broadcast
+        .to(message.room)
+        .emit('message', message)
     }
   })
+})
 
-  /**
-   *  If the SPECIFIC SOCKET sends join, then it leaves object: previous room and
-   *  joins a new room. Then emit joinResult to the room 'newRoom'.
-   */
-  socket.on('join', (room) => {
-    socket.leave(room.previousRoom)
-    socket.join(room.newRoom)
-    socket.emit('joinResult', {
-      room: room.newRoom
+const io = require('socket.io').listen(server)
+const socketio = require('socket.io-client')('http://localhost:3001')
+
+const Parser = require('./parser')
+const parser = new Parser(io)
+
+io.on('connection', socket => {
+    console.log(socket.id, 'connected')
+    socketio.emit('message', 'Hello we are the chat server!')
+
+    socket.on('disconnect', () => {
+        console.log(socket.id, 'disconnected')
     })
-
-  })
-
-  socket.on('message', (message) => {
-    message.nick = nickNames[socket.id]
-    message.id = socket.id
-    console.log(message)
-    socket
-      .broadcast
-      .to(message.room)
-      .emit('message', {
-        text: nickNames[socket.id] + ': ' + message.text
-      })
-  })
-
+    socket.on('message', message => {
+        console.log(message)
+        if (message.text[0] === '/') {
+            parser.readInput(message)
+        } else {
+            socket
+                .broadcast
+                .to(message.room)
+                .emit('message', message)
+        }
+    })
 })
