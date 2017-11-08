fb44fad84c17d349b96ae9cfd931197d84f7cb57
Cameron Green <cgree78@gmail.com>
2017-11-08 12:27:07 -0600

    removed duplicate code

--- backEnd/index.js
+++ backEnd/index.js
@@ -15,32 +15,6 @@ const Parser = require('./parser')
 const parser = new Parser(io)
 
 io.on('connection', socket => {
-  console.log(socket.id, 'connected')
-  // parser.readInput(socket)
-  socketio.emit('message', 'hello we are back end!')
-
-  socket.on('disconnect', () => {
-    console.log(socket.id, 'disconnected')
-  })
-  socket.on('message', message => {
-    if (message.text[0] === '/') {
-      parser.readInput(message)
-    } else {
-      socket
-        .broadcast
-        .to(message.room)
-        .emit('message', message)
-    }
-  })
-})
-
-const io = require('socket.io').listen(server)
-const socketio = require('socket.io-client')('http://localhost:3001')
-
-const Parser = require('./parser')
-const parser = new Parser(io)
-
-io.on('connection', socket => {
     console.log(socket.id, 'connected')
     socketio.emit('message', 'Hello we are the chat server!')
 
