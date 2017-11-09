7356d59d02390e4c5c36c761638a62c5a8e5646d
Cameron Green <cgree78@gmail.com>
2017-11-08 18:26:05 -0600

    changed the server location on front end

--- backEnd/parser.js
+++ backEnd/parser.js
@@ -1,16 +1,24 @@
 module.exports = class Parser {
     readInput(message) {
-        let words = message.text.split(' ')
+        let words = message
+            .text
+            .split(' ')
         let command = words[0]
             .substring(1, words[0].length)
             .toLowerCase()
         switch (command) {
             case 'join':
+                console.log("join command")
                 break
             case 'nick':
+                console.log("nick command")
+                break
+            case 'python':
+                console.log("python command")
                 break
             default:
-                message = 'Unrecognized Command.'
+                console.log("unrecognized command")
+                // message.text = 'Unrecognized Command.'
                 break
         }
     }
