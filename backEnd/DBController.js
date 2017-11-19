faf78794e94fde4b15d60359457c083a51797f67
Cameron Green <cgree78@gmail.com>
2017-11-19 17:47:38 -0600

    fixed more merge conflicts lol

--- backEnd/DBController.js
+++ backEnd/DBController.js
@@ -11,27 +11,6 @@ const redisController = new redisControlFile()
      */
 
 module.exports = class DBController {
-    /**
-     * run command to connect to the PostgreSQL Database
-     */
-    connect() {
-        dbcommands.connectDB()
-    }
-    /**
-     * If the input is a 'message', then send the message() to DBCOmmands and redisCOntroller
-     *      which resolves into a message saving into both DB
-     * If the input is 'command', then send the command() to Abstraction
-     *      which resolves into a String Object to send to sandbox
-     * @param {Request} req
-     * @param {Response} res
-     */
-    get(req, res) {
-        let command = req.query.cmd
-        let message = req.query.message
-
-
-
-module.exports = class DBC {
 
     save(message) {
         redisController.cacheMessage(message)
