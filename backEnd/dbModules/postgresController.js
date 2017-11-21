2991d34088bac1ced33a172375af7dfb3f03462a
Cameron Green <cgree78@gmail.com>
2017-11-21 16:27:17 -0600

    put the new user function inside the class

--- backEnd/dbModules/postgresController.js
+++ backEnd/dbModules/postgresController.js
@@ -2,27 +2,29 @@
 
 //stevan & sabian queries
 const promise = require('bluebird')
-const pgp = require('pg-promise')({promiseLib: promise})
+const pgp = require('pg-promise')({
+    promiseLib: promise
+})
 const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/codechat'
 const client = pgp(connectionString)
 
 module.exports = class PostgresController {
     async testDB() {}
-//}
-//modules.exports = class DBCommands {
+    //}
+    //modules.exports = class DBCommands {
 
-	//will connect to DB
+    //will connect to DB
     connectBD() {
         client.connect((err) => {
-            if(err) {
+            if (err) {
                 console.error('connection error', err.stack)
             } else {
                 console.log('Connected to Postgres')
-        }
-    })
+            }
+        })
     }
-	
-	//disconnect from  DB
+
+    //disconnect from  DB
     disconnectDB() {
         client.end((err) => {
             if (err) {
@@ -32,21 +34,21 @@ module.exports = class PostgresController {
             }
         })
     }
-}
 
-//1) insert a New User record (registers with website)
+    //1) insert a New User record (registers with website)
     insert_New_User(u_id, u_email, u_pass, u_username, u_firstname, u_lastname, u_bio) {
-		{
-    client
-		.any(`INSERT INTO "Users"(u_username", "u_pass", "u_email", "u_id", "u_firstname", "u_lastname", "u_bio") VALUES ('${u_username}', '${u_pass}', '${u_email}',  '${u_id}', '${u_firstname}", '${U_lastname}', '${u_bio}') returning u_username`)
-        .then(() => {
-            reslove("Successful Insert of new user.")
-        })
-		}
-	}
-	/* insert message into DB, not finished.
-	insert_Message(m_id, ch_id, u_id, message_text, isCode, m_response, time, hasInputs){
-		Client
-		.any(`INSERT INTO `)
-	}
+        {
+            client
+                .any(`INSERT INTO "Users"(u_username", "u_pass", "u_email", "u_id", "u_firstname", "u_lastname", "u_bio") VALUES ('${u_username}', '${u_pass}', '${u_email}',  '${u_id}', '${u_firstname}", '${U_lastname}', '${u_bio}') returning u_username`)
+                .then(() => {
+                    reslove("Successful Insert of new user.")
+                })
+        }
+    }
+    /* insert message into DB, not finished.
+    insert_Message(m_id, ch_id, u_id, message_text, isCode, m_response, time, hasInputs){
+        Client
+        .any(`INSERT INTO `)
+    }
     */
+}
