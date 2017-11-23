c668065a78cdf0e374feb8b1193f465371afbc4f
Cameron Green <cgree78@gmail.com>
2017-11-22 22:58:24 -0600

    fixed some bugs

--- backEnd/socketModules/virtualizationCommands.js
+++ backEnd/socketModules/virtualizationCommands.js
@@ -1,16 +1,20 @@
 'use strict'
 
 const WebSocket = require('ws')
-const ws = new WebSocket("ws://104.131.129.223:3003")
+// const ws = new WebSocket("ws://104.131.129.223:3003")
 
-ws.addEventListener('open', (event) => {
-    ws.send('Hai Server!', (err, res) => {
-        if (err)
-            console.log("Error:", err)
-    })
-})
+module.exports = class VirtualizationCommands {
+    constructor() {
+        // ws.addEventListener('open', (event) => {
+        //     ws.send('Hai Server!', (err, res) => {
+        //         if (err)
+        //             console.log("Error:", err)
+        //     })
+        // })
 
-// Listen for messages
-ws.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data)
-})
+        // // Listen for messages
+        // ws.addEventListener('message', (event) => {
+        //     console.log('Message from server ', event.data)
+        // })
+    }
+}
