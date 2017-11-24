90efd0b36c401c45da9687b809bf2de20828f8bb
yesenia <roflicer@Yesenias-MacBook-Pro.local>
2017-11-23 23:30:02 -0600

    added classes to returns

--- frontEnd/src/components/SignIn/SignPage.js
+++ frontEnd/src/components/SignIn/SignPage.js
@@ -5,7 +5,6 @@ import TextField from 'material-ui/TextField';
 
 import React, { Component } from 'react';
 import './SignPage.css';
-//import GoogleLogin from 'react-google-login';
 
 class Login extends Component {
 constructor(props){
@@ -18,6 +17,7 @@ constructor(props){
 
 render() {
     return (
+      <div className="SignPage">
       <div>
         <MuiThemeProvider>
           <div>
@@ -43,44 +43,10 @@ render() {
          </div>
          </MuiThemeProvider>
       </div>
+      </div>
     );
 
   }
-
-
-/*
-  handleClick(event){
-    var apiBaseUrl = "http://localhost:4000/api/";
-    var self = this;
-    var payload={
-    "email":this.state.username,
-    "password":this.state.password
-    }
-    axios.post(apiBaseUrl+'login', payload)
-    .then(function (response) {
-    console.log(response);
-    if(response.data.code == 200){
-    console.log("Login successfull");
-    var uploadScreen=[];
-    uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
-    self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
-    }
-    else if(response.data.code == 204){
-    console.log("Username password do not match");
-    alert("username password do not match")
-    }
-    else{
-    console.log("Username does not exists");
-    alert("Username does not exist");
-    }
-    })
-    .catch(function (error) {
-    console.log(error);
-    });
-    }
-*/
-
-
 }
 
 
