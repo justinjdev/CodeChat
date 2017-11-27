b64da377280823ec608925fe5b9438086c2253b4
Cameron Green <cgree78@gmail.com>
2017-11-27 10:49:45 -0600

    redis pls

--- backEnd/dbModules/redisController.js
+++ backEnd/dbModules/redisController.js
@@ -12,6 +12,7 @@ module.exports = class redisController {
     }
     async cacheMessage(message) {
         let room = message.room
+        console.log("cacheing message for room:",room)
         //get length of the specific room name
         redisClient.llen(`${room}`, (err, len) => {
             if (len >= 50) {    // if length is > 20 (it's initialized at 1) then just get rid of the old one
@@ -21,10 +22,12 @@ module.exports = class redisController {
         redisClient.rpush(`${room}`, JSON.stringify(message))
     }
     async getCachedMessages(roomName) {
+        console.log("getting cache for room name:",roomName)
         redisClient.LRANGE(`${roomName}`, 0, -1, (err, messages) => {
-            // console.log("roomName", roomName, "Messages:", messages)
+            console.log("roomName", roomName, "Messages:", messages)
             this.messages = messages
         })
+        console.log("returning cache")
         return(this.messages)
     }
 
