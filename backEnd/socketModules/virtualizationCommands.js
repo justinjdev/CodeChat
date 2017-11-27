8a65e30f8fe8b6af7e6533682ce3109d40f659f3
Cameron Green <cgree78@gmail.com>
2017-11-27 00:33:56 -0600

    compler works.

--- backEnd/socketModules/virtualizationCommands.js
+++ backEnd/socketModules/virtualizationCommands.js
@@ -2,34 +2,39 @@
 
 const WebSocket = require('ws')
 const ws = new WebSocket("ws://104.131.129.223:3003")
-
-// const socketfile = require('./Socket') const socketio =  Proxy( new
-// socketfile())
+// const websocketServerLocation = ("ws://104.131.129.223:3003") const
+// socketfile = require('./Socket') const socketio =  Proxy( new socketfile())
 
 const ObjectResponseFile = require('./ObjectResponse')
 const objectResponse = (new ObjectResponseFile())
 
 module.exports = class VirtualizationCommands {
     constructor(io) {
-        ws.addEventListener('open', (event) => {
+        ws.onopen = (event) => {
             console.log("opened websocket connection")
-        })
+        }
+        // this.start(websocketServerLocation)
         this.serverResponse = 'default'
         this.objectResponse = new ObjectResponseFile(io)
-        // Listen for messages
-        // ws.addEventListener('message', (event) => {
+        // Listen for messages ws.addEventListener('message', (event) => {
         // console.log('Message from server ', event.data) })
+        ws.onclose = (e) => {
+            // console.log(e)
+            console.log(e.code)
+            console.log("closed connection")
+        }
     }
-    //taking language argument
+
     async language(message) {
         let msg = JSON.stringify(message)
-        console.log("msg", msg)
+        // console.log("msg", msg)
         ws.send(msg, (err, res) => {
             if (err)
                 console.log("Websocket send Error:", err)
             else {
-                (console.log(res))
+                console.log(res)
             }
+            return false;
         })
         // Listen for messages
         ws.addEventListener('message', event => {
@@ -38,17 +43,6 @@ module.exports = class VirtualizationCommands {
                 .objectResponse
                 .sendResponseToFrontEnd(event.data)
         })
-        return ('sent')
-
+        return (message)
     }
-    // ignorethis() {
-    //     ws.addEventListener('open', (event) => {
-    //         ws.send('Hi Server!', (err, res) => {
-    //             if (err)
-    //                 console.log("Error:", err)
-    //         })
-    //     })
-
-    // }
-
 }
