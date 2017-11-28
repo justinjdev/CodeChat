b95349058df893b27c9f717e21060720001631f3
Cameron Green <cgree78@gmail.com>
2017-11-28 14:10:35 -0600

    fixed port 80 breaking

--- frontEnd/src/components/App/App.js
+++ frontEnd/src/components/App/App.js
@@ -8,12 +8,42 @@ import Audio from '../Audio/Audio'
 
 import io from 'socket.io-client'
 
-//const socket = io('104.131.129.223:8080') // servers
+// const socket = io('104.131.129.223:8080') // servers
 const socket = io('localhost:8080') // servers
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      register: false,
+      chatting: true
+    }
+  }
+  getRegister() {
+    const register = localStorage.getItem('registered')
+    let state = this.state
+    state.register = register
+    this.setState(state)
+  }
+
+  getSignedIn() {
+    const chatting = localStorage.getItem('chatting')
+    let state = this.state
+    state.chatting = chatting
+    this.setState(state)
+  }
+
+
   render() {
-    return (<MainPage {...this.props} socket={socket} />)
+    /**
+     * If signed in is true, then show main page.
+     *  else, if register is true, show register page, otherwise show sign in page
+     */
+    return (this.state.chatting
+      ? <MainPage {...this.props} socket={socket}/>
+      : this.state.register
+        ? <Register {...this.props} socket={socket}/>
+        : <SignIn {...this.props} socket={socket}/>)
   }
 }
 
