6c77ab3d58354daee0a5cab794e75b4f6ff65373
Cameron Green <cgree78@gmail.com>
2017-11-28 19:26:31 -0600

    server should work over https?

--- backEnd/index.js
+++ backEnd/index.js
@@ -5,12 +5,15 @@ const http = require('https')
 
 app.set('port', process.env.PORT || 8080)
 
-let server = app.listen(app.get('port'), () => {
+let server = http.createServer(app)
+const io = require('socket.io').listen(server) //listen to own server
+// const socketio = require('socket.io-client')('localhost:3001') //listen to justin server
+
+
+server.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'))
 })
 
-const io = require('socket.io').listen(server) //listen to own server
-const socketio = require('socket.io-client')('localhost:3001') //listen to justin server
 // const ioAudio = require('socket.io').listen(server) //listen to own server
 
 
